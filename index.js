d644261441bf43113a905ecb8c25fcd9c8c6cadb
auyber <160794306+auyber@users.noreply.github.com>
2025-02-23 11:55:21 -0300

    feat: add effects on pages

--- index.js
+++ index.js
@@ -1,41 +1,48 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Inicializa o Swiper da Home
-  new Swiper(".mySwiper", {
-      slidesPerView: 1,
-      spaceBetween: 10,
-      loop: true,
-      navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-      },
-      pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-      },
-      autoplay: {
-          delay: 3000,
-          disableOnInteraction: false,
-      },
+    // Inicializa o Swiper da Home
+    new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        loop: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+    });
+  
+    // Inicializa todos os Swipers da seção de serviços
+    document.querySelectorAll(".mySwiperServices").forEach(swiperContainer => {
+        new Swiper(swiperContainer, {
+            slidesPerView: 1,
+            spaceBetween: 10,
+            loop: true,
+            navigation: {
+                nextEl: swiperContainer.querySelector(".swiper-button-next"),
+                prevEl: swiperContainer.querySelector(".swiper-button-prev"),
+            },
+            pagination: {
+                el: swiperContainer.querySelector(".swiper-pagination"),
+                clickable: true,
+            },
+            autoplay: {
+                delay: 3000,
+                disableOnInteraction: false,
+            },
+        });
+    });
+  
+    // Inicializa o AOS (Animate On Scroll)
+    AOS.init({
+      duration: 1000,
+      once: true,
+      easing: 'ease-in-out'
+    });
   });
-
-  // Inicializa todos os Swipers da seção de serviços
-  document.querySelectorAll(".mySwiperServices").forEach(swiperContainer => {
-      new Swiper(swiperContainer, {
-          slidesPerView: 1,
-          spaceBetween: 10,
-          loop: true,
-          navigation: {
-              nextEl: swiperContainer.querySelector(".swiper-button-next"),
-              prevEl: swiperContainer.querySelector(".swiper-button-prev"),
-          },
-          pagination: {
-              el: swiperContainer.querySelector(".swiper-pagination"),
-              clickable: true,
-          },
-          autoplay: {
-              delay: 3000,
-              disableOnInteraction: false,
-          },
-      });
-  });
-});
